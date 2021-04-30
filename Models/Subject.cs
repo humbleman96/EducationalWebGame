d4501520d476d219cfefe95899fc51f5e2449e84
humbleman96 <humbleman@abv.bg>
2021-04-30 12:35:15 +0300

    Add files via upload

---
+++ Models/Subject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducationalWebGame.Models
+{
+    public class Subject
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string SubjectName { get; set; }
+
+        public ICollection<Game> Games { get; set; }
+
+    }
+}
