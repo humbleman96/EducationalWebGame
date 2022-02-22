eace66227e01ade7b4ede3519ef0b002abe7bf12
Stanimir Tonev <stan.tonev96@gmail.com>
2022-02-22 22:57:33 +0200

    Upload EducationalWebGame

---
+++ EducationalWebGame/Models/ResourceTeacher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducationalWebGame.Models
+{
+    public class ResourceTeacher : User
+    {        
+        public ICollection<Student> Students { get; set; }
+    }
+}
---
+++ EducationalWebGame/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducationalWebGame.Models
+{
+    public class Student : User
+    {
+        [Required]
+        public uint Coins { get; set; }
+
+        [Required]
+        public uint Points { get; set; }
+
+        [Required]
+        public uint Lives { get; set; }
+
+        public int? TeacherId { get; set; }
+        public Teacher Teacher { get; set; }
+
+        public int? ResourceTeacherId { get; set; }
+        public ResourceTeacher ResourceTeacher { get; set; }
+
+        public ICollection<StudentGame> StudentGames { get; set; }
+    }
+}
